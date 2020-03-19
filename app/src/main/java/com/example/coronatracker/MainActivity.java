cca8c74bca5b8d6a5938a1d7fe566f96f49db081
Lalit <wazirlalit96420@gmail.com>
2020-03-19 15:14:57 +0530

    populating list view with sample data

--- app/src/main/java/com/example/coronatracker/MainActivity.java
+++ app/src/main/java/com/example/coronatracker/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.coronatracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Fake corona cases locations.
+        ArrayList<String> coronaCases = new ArrayList<>();
+        coronaCases.add("San Francisco");
+        coronaCases.add("London");
+        coronaCases.add("Tokyo");
+        coronaCases.add("Mexico City");
+        coronaCases.add("Moscow");
+        coronaCases.add("Rio de Janeiro");
+        coronaCases.add("Paris");
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView CoronaListView = (ListView) findViewById(R.id.list);
+
+        // Create a new {@link ArrayAdapter} of corona cases
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, coronaCases);
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        CoronaListView.setAdapter(adapter);
+
     }
 }
