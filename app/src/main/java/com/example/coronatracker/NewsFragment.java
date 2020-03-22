660e3584f7206bfdb5a81fd31a94d5246268d07c
Lalit <wazirlalit96420@gmail.com>
2020-03-23 00:10:33 +0530

    added Tab Layout and added self diagnosis bot

--- app/src/main/java/com/example/coronatracker/NewsFragment.java
+++ app/src/main/java/com/example/coronatracker/NewsFragment.java
@@ -1,21 +1,22 @@
 package com.example.coronatracker;
-
-
-import android.app.LoaderManager;
-import android.app.LoaderManager.*;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;;
 import android.content.Context;
 import android.content.Intent;
-import android.content.Loader;
+
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -23,12 +24,18 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
-public class MainActivity extends AppCompatActivity
+public class NewsFragment extends Fragment
         implements LoaderManager.LoaderCallbacks<List<CoronaNews>> {
 
-    private static final String LOG_TAG = MainActivity.class.getName();
-
+    private static final String LOG_TAG = NewsFragment.class.getName();
+    android.support.v4.app.LoaderManager loaderManager = null;
     /** URL for corona data from the News API */
     private static final String CORONA_NEWS_REQUEST_URL ="https://news.google.com/rss/search?q=Corona%20Case%20update%20in%20india&hl=en-IN&gl=IN&ceid=IN:en";
 
@@ -45,20 +52,36 @@ public class MainActivity extends AppCompatActivity
 
     /** TextView that is displayed when the list is empty */
     private TextView mEmptyStateTextView;
+    public NewsFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static NewsFragment newInstance(String param1, String param2) {
+        NewsFragment fragment = new NewsFragment();
+
+        return fragment;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_news, container, false);
         // Find a reference to the {@link ListView} in the layout
-        ListView CoronaNewsListView = (ListView) findViewById(R.id.list);
+        ListView CoronaNewsListView = (ListView) v.findViewById(R.id.list);
 
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        mEmptyStateTextView = (TextView) v.findViewById(R.id.empty_view);
         CoronaNewsListView.setEmptyView(mEmptyStateTextView);
 
         // Create a new adapter that takes an empty list of corona news as input
-        mAdapter = new CoronaNewsAdapter(this, new ArrayList<CoronaNews>());
+        mAdapter = new CoronaNewsAdapter(getContext(), new ArrayList<CoronaNews>());
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
@@ -85,7 +108,7 @@ public class MainActivity extends AppCompatActivity
 
         // Get a reference to the ConnectivityManager to check state of network connectivity
         ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
+                getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
 
         // Get details on the currently active default data network
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
@@ -93,36 +116,37 @@ public class MainActivity extends AppCompatActivity
         // If there is a network connection, fetch data
         if (networkInfo != null && networkInfo.isConnected()) {
             // Get a reference to the LoaderManager, in order to interact with loaders.
-            android.app.LoaderManager loaderManager = getLoaderManager();
+          loaderManager = getLoaderManager();
 
             // Initialize the loader. Pass in the int ID constant defined above and pass in null for
             // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
             // because this activity implements the LoaderCallbacks interface).
 
-            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+         loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
         } else {
             // Otherwise, display error
             // First, hide loading indicator so error message will be visible
-            View loadingIndicator = findViewById(R.id.loading_indicator);
+            View loadingIndicator = v.findViewById(R.id.loading_indicator);
             loadingIndicator.setVisibility(View.GONE);
 
             // Update empty state with no connection error message
             mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
-
+        // Inflate the layout for this fragment
+        return v;
     }
 
 
-
+    @NonNull
     @Override
-    public Loader<List<CoronaNews>> onCreateLoader(int id, Bundle args) {
-        return new CoronaNewsLoader(this, CORONA_NEWS_REQUEST_URL);
+    public Loader<List<CoronaNews>> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new CoronaNewsLoader(getContext(), CORONA_NEWS_REQUEST_URL);
     }
 
     @Override
-    public void onLoadFinished(Loader<List<CoronaNews>> loader, List<CoronaNews> data) {
+    public void onLoadFinished(@NonNull Loader<List<CoronaNews>> loader, List<CoronaNews> data) {
         // Hide loading indicator because the data has been loaded
-        View loadingIndicator = findViewById(R.id.loading_indicator);
+        View loadingIndicator = getView().findViewById(R.id.loading_indicator);
         loadingIndicator.setVisibility(View.GONE);
 
         // Set empty state text to display "No Corona News Found found."
@@ -138,31 +162,8 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onLoaderReset(Loader<List<CoronaNews>> loader) {
+    public void onLoaderReset(@NonNull Loader<List<CoronaNews>> loader) {
         // Loader reset, so we can clear out our existing data.
         mAdapter.clear();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.mbEmbeddedIndiaStateWiseWebView) {
-            Intent settingsIntent = new Intent(this, india_statewiise_corona_cases.class);
-            startActivity(settingsIntent);
-            return true;
-        }
-        else if (id == R.id.mbEmbeddedWebView)
-        {
-            Intent settingsIntent = new Intent(this, web_view_for_corona_map.class);
-            startActivity(settingsIntent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
